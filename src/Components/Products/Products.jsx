e85892c6ef1ab76966a856b293dddadbddf822cf
yas3jb <yasmu213@gmail.com>
2024-03-27 21:59:49 -0400

    Added useNavigate to navigate to singleProduct page

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,14 +1,22 @@
 import { useState, useEffect } from "react";
 // Import fetchProducts function
 import { fetchProducts } from "../../API/api.js";
+// Import useNavigate
+import { useNavigate } from "react-router-dom";
 
 export default function Products() {
-  const [products, setProducts] = useState([]); // state variable
+  // State variable to store the products
+  const [products, setProducts] = useState([]);
+  // Hook for navigation
+  const navigate = useNavigate();
 
+  // Effect hook to fetch products
   useEffect(() => {
+    // Fetching products from API
     fetchProducts()
       .then((products) => {
-        setProducts(products); // Update the 'products' state variable
+        // Update the 'products' state variable
+        setProducts(products);
       })
       .catch((error) => {
         console.error("Error fetching products:", error);
@@ -24,7 +32,9 @@ export default function Products() {
           <p>{product.description}</p>
           <h4>{product.price}</h4>
           <button>Add</button>
-          <button>View</button>
+          <button onClick={() => navigate(`/products/${product.id}`)}>
+            View
+          </button>
         </div>
       ))}
     </>
