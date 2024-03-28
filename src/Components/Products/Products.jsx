fa01064fea905e9107e886fe8849b78dbbc267ac
yas3jb <yasmu213@gmail.com>
2024-03-27 21:26:47 -0400

    Implement product listing and rendering functionality

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,3 +1,32 @@
+import { useState, useEffect } from "react";
+// Import fetchProducts function
+import { fetchProducts } from "../../API/api.js";
+
 export default function Products() {
-  return <div>Products</div>;
+  const [products, setProducts] = useState([]); // state variable
+
+  useEffect(() => {
+    fetchProducts()
+      .then((products) => {
+        setProducts(products); // Update the 'products' state variable
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      {products.map((product) => (
+        <div key={product.id}>
+          <img src={product.image} alt="Product Image" />
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <h4>{product.price}</h4>
+          <button>Add</button>
+          <button>View</button>
+        </div>
+      ))}
+    </>
+  );
 }
