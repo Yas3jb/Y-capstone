3636e46c43f4db8d5d725e62b27d2d9e5cdae235
yas3jb <yasmu213@gmail.com>
2024-03-27 20:46:36 -0400

    Implemented Navbar, Products, SingleProduct, Register and Login components with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Import Components
+
+import { Routes, Route } from "react-router-dom";
+import Products from "./Components/Products/Products";
+import SingleProduct from "./Components/SingleProduct/SingleProduct";
+import Register from "./Components/Register/Register";
+import Login from "./Components/Login/Login";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // Render Components
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">SHOP</Link>{" "}
+          </li>
+          <li>
+            <Link to="/register">REGISTER</Link>{" "}
+          </li>
+          <li>
+            <Link to="/login">LOGIN</Link>{" "}
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
