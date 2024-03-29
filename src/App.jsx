167800fe0b4b7ce7de4eb404c96d301bc4ad5cb2
yas3jb <yasmu213@gmail.com>
2024-03-29 16:11:17 -0400

    Imported Jewelery Component and setup the route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SingleProduct from "./Components/SingleProduct/SingleProduct";
 import Register from "./Components/Register/Register";
 import Login from "./Components/Login/Login";
 import Navbar from "./Components/Navbar/Navbar";
+import Jewelery from "./Components/Jewelery/Jewelery";
 
 function App() {
   // Render Components
@@ -16,6 +17,7 @@ function App() {
         <Route path="/products/:id" element={<SingleProduct />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/products/category/jewelery" element={<Jewelery />} />
       </Routes>
     </>
   );
