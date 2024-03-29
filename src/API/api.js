e5cae346702c3df36ae74d1676ddceb5a6cee3ed
yas3jb <yasmu213@gmail.com>
2024-03-28 22:07:04 -0400

    Create user added

--- src/API/api.js
+++ src/API/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 // DATA URL
 const URL = "https://fakestoreapi.com";
+const Base_URL = "http://localhost:3000/api";
 
 // Fetch Products
 export const fetchProducts = async () => {
@@ -21,5 +22,29 @@ export const fetchSingleProduct = async (id) => {
     return response.data;
   } catch (err) {
     console.error(err);
+  }
+};
+
+// Create User
+export const createUser = async (credentials) => {
+  try {
+    const response = await axios.post(
+      `${Base_URL}/auth/register`,
+      credentials,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response.status === 200) {
+      const { token } = response.data;
+      window.localStorage.setItem("token", token);
+      console.log(response.data);
+    } else {
+      console.error(response.data);
+    }
+  } catch (error) {
+    console.error("Error:", error);
   }
 };
